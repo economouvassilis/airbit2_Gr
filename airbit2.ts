6e1d42fce6ff6cc4051b7ac03c25caa285303f30
Economou Vassilis <economou.v@gmail.com>
2026-02-07 07:13:24 +0200

    Refactor enums and update function parameters

--- airbit2.ts
+++ airbit2.ts
@@ -4,8 +4,8 @@
  * Παρέχει απλοποιημένες εντολές στα Ελληνικά για τον έλεγχο του drone.
  */
 
-// 1. ΟΡΙΣΜΟΣ ΤΩΝ ENUMS ΕΚΤΟΣ NAMESPACE ΓΙΑ ΝΑ ΕΜΦΑΝΙΣΤΟΥΝ ΤΑ COMBO BOXES
-enum Κατεύθυνση {
+// 1. Ορισμός Enums με διαφορετικό όνομα από τις παραμέτρους για να εμφανιστούν τα μενού
+enum MoveDir {
     //% block="Μπροστά"
     Forward,
     //% block="Πίσω"
@@ -16,7 +16,7 @@ enum Κατεύθυνση {
     Down
 }
 
-enum ΔεξιάΑριστερά {
+enum TurnDir {
     //% block="Δεξιά"
     Right,
     //% block="Αριστερά"
@@ -26,10 +26,6 @@ enum ΔεξιάΑριστερά {
 //% weight=100 color=#00AEEF icon="\uf140" block="AirBit Ελληνικά"
 namespace airbit2_GR {
 
-    /**
-     * Αρχικοποίηση του drone. Προετοιμάζει τους αισθητήρες και καλιμπράρει το γυροσκόπιο.
-     * Το drone πρέπει να είναι σε επίπεδη επιφάνεια!
-     */
     //% block="Αρχικοποίηση"
     export function αρχικοποίηση() {
         airbit.IMU_Start()
@@ -40,22 +36,15 @@ namespace airbit2_GR {
         basic.showIcon(IconNames.Happy)
     }
 
-    /**
-     * Απογείωση και σταθεροποίηση.
-     */
     //% block="Απογείωση"
     export function απογείωση() {
         arm = 1
-        // Σταδιακή αύξηση ισχύος για ομαλή απογείωση
         for (let i = 0; i <= 65; i++) {
             throttle = i
             basic.pause(20)
         }
     }
 
-    /**
-     * Προσγείωση του drone με σταδιακή μείωση της ισχύος.
-     */
     //% block="Προσγείωση"
     export function προσγείωση() {
         for (let i = throttle; i >= 0; i--) {
@@ -67,43 +56,29 @@ namespace airbit2_GR {
         basic.showIcon(IconNames.No)
     }
 
-    /**
-     * Κίνηση προς μια κατεύθυνση.
-     * @param κατεύθυνση επιλογή από μενού
-     * @param cm εκατοστά κίνησης (εκτίμηση βάσει χρόνου/ισχύος)
-     */
     //% block="Κινήσου %κατεύθυνση για %cm εκατοστά"
     //% cm.defl=50
-    export function κίνηση(κατεύθυνση: Κατεύθυνση, cm: number) {
-        let ms = cm * 25; // Χονδρική εκτίμηση: 25ms ανά εκατοστό
-        
-        if (κατεύθυνση == Κατεύθυνση.Forward) {
-            pitch = -10 // Κλίση μπροστά
-        } else if (κατεύθυνση == Κατεύθυνση.Backward) {
-            pitch = 10  // Κλίση πίσω
-        } else if (κατεύθυνση == Κατεύθυνση.Up) {
-            throttle += 10 // Αύξηση ώσης
-        } else if (κατεύθυνση == Κατεύθυνση.Down) {
-            throttle -= 10 // Μείωση ώσης
+    export function κίνηση(κατεύθυνση: MoveDir, cm: number) {
+        let ms = cm * 25;
+        if (κατεύθυνση == MoveDir.Forward) {
+            pitch = -10
+        } else if (κατεύθυνση == MoveDir.Backward) {
+            pitch = 10
+        } else if (κατεύθυνση == MoveDir.Up) {
+            throttle += 10
+        } else if (κατεύθυνση == MoveDir.Down) {
+            throttle -= 10
         }
-        
         basic.pause(ms)
-        
-        // Επαναφορά σε επίπεδη πτήση
         pitch = 0
         throttle = Math.constrain(throttle, 0, 100)
     }
 
-    /**
-     * Στροφή του drone γύρω από τον άξονά του (Yaw).
-     * @param στροφή επιλογή Δεξιά ή Αριστερά
-     * @param μοίρες μοίρες στροφής
-     */
     //% block="Στρίψε %στροφή %μοίρες μοίρες"
     //% μοίρες.min=0 μοίρες.max=360
-    export function στροφή(στροφή: ΔεξιάΑριστερά, μοίρες: number) {
-        let στροφή_ms = μοίρες * 5; // Εκτίμηση χρόνου περιστροφής
-        if (στροφή == ΔεξιάΑριστερά.Right) {
+    export function στροφή(στροφή: TurnDir, μοίρες: number) {
+        let στροφή_ms = μοίρες * 5;
+        if (στροφή == TurnDir.Right) {
             yaw += μοίρες
         } else {
             yaw -= μοίρες
@@ -111,23 +86,15 @@ namespace airbit2_GR {
         basic.pause(στροφή_ms)
     }
 
-    /**
-     * Εμφανίζει στην οθόνη LED την κατάσταση του drone.
-     */
     //% block="Προβολή Πληροφοριών"
     export function προβολήΠληροφοριών() {
         basic.clearScreen()
-        // Μπάρα μπαταρίας
         airbit.smartBar(4, airbit.batteryLevel())
-        // Κουκίδα κλίσης (Pitch/Roll)
         let ledX = Math.map(imuRoll, -15, 15, 0, 4)
         let ledY = Math.map(imuPitch, -15, 15, 4, 0)
         led.plot(ledX, ledY)
     }
 
-    /**
-     * Επιστρέφει το ποσοστό της μπαταρίας.
-     */
     //% block="Επίπεδο Μπαταρίας"
     export function ποσοστόΜπαταρίας(): number {
         return airbit.batteryLevel()
